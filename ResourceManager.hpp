a22f2673c651dbc0b3f4920643f45f053bfe4a55
MilosK2 <jano9987@gmail.com>
2024-10-31 23:05:22 +0100

    Final 2laby

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,19 +4,27 @@
 
 class ResourceManager
 {
+    public:
     ResourceManager() { resource_ = new Resource();
     }
-    ResourceManager(const ResourceManager& resMan) { resource_ = resMan.resource_;
+        ResourceManager(const ResourceManager& resMan)
+        {
+        resource_ = new Resource();
+        *resource_ = *resMan.resource_;
     }
     
     ResourceManager& operator=(const ResourceManager& resMan)
     { 
+        *this->resource_ = *resMan.resource_;
         return *this;
     }
 
     ~ResourceManager() {
-
+        if (this->resource_ != nullptr) {
+            delete this->resource_;
+        }
     }
+
     double get()
     {
         return resource_->get();
@@ -24,3 +32,4 @@ class ResourceManager
 
     Resource* resource_=nullptr;
 };
+
