20db1d73fc62a291ebbbccfce613359ed2060d44
MilosK2 <jano9987@gmail.com>
2024-12-09 15:11:26 +0100

    Semantyka

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,25 +5,38 @@
 class ResourceManager
 {
     public:
-    ResourceManager() { resource_ = new Resource();
-    }
+        ResourceManager() { resource_ = new Resource(); }
+
         ResourceManager(const ResourceManager& resMan)
         {
-        resource_ = new Resource();
-        *resource_ = *resMan.resource_;
-    }
-    
-    ResourceManager& operator=(const ResourceManager& resMan)
-    { 
+            resource_  = new Resource();
+            *resource_ = *resMan.resource_;
+        }
+
+        ResourceManager& operator=(const ResourceManager& resMan)
+        {
         *this->resource_ = *resMan.resource_;
         return *this;
-    }
+        }
+        ResourceManager(const ResourceManager&& resMan)
+        {
+        resource_  = new Resource();
+        *resource_ = *resMan.resource_;
+        }
+
+        ResourceManager& operator=(const ResourceManager&& resMan)
+        {
+        *this->resource_ = *resMan.resource_;
+        return *this;
+        }
 
     ~ResourceManager() {
         if (this->resource_ != nullptr) {
             delete this->resource_;
         }
     }
+
+
 
     double get()
     {
